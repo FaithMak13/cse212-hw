5b7679877deac20d4a4c40ef75855b0355b2c596
Faith <fmakawure@byupathway.edu>
2026-06-15 07:05:53 +0200

    Week 03 updated

--- Week03/MazeMovementFunctions.cs
+++ Week03/MazeMovementFunctions.cs
@@ -1,13 +1,45 @@
-Assuming:
-maze is Dictionary<(int x, int y), (bool left, bool right, bool up, bool down)>
-current is a (int x, int y) position
+using System.Collections.Generic;
 
-public (int x, int y) MoveLeft((int x, int y) current)
+static class MazeMovementFunctions
 {
-    var cell = maze[current];
+    // Maze is Dictionary<(int x, int y), (bool left, bool right, bool up, bool down)>
+    public static (int x, int y) MoveLeft(Dictionary<(int x, int y), (bool left, bool right, bool up, bool down)> maze, (int x, int y) current)
+    {
+        var cell = maze[current];
 
-    if (cell.left)
-        return (current.x - 1, current.y);
+        if (cell.left)
+            return (current.x - 1, current.y);
 
-    return current;
+        return current;
+    }
+
+    public static (int x, int y) MoveRight(Dictionary<(int x, int y), (bool left, bool right, bool up, bool down)> maze, (int x, int y) current)
+    {
+        var cell = maze[current];
+
+        if (cell.right)
+            return (current.x + 1, current.y);
+
+        return current;
+    }
+
+    public static (int x, int y) MoveUp(Dictionary<(int x, int y), (bool left, bool right, bool up, bool down)> maze, (int x, int y) current)
+    {
+        var cell = maze[current];
+
+        if (cell.up)
+            return (current.x, current.y - 1);
+
+        return current;
+    }
+
+    public static (int x, int y) MoveDown(Dictionary<(int x, int y), (bool left, bool right, bool up, bool down)> maze, (int x, int y) current)
+    {
+        var cell = maze[current];
+
+        if (cell.down)
+            return (current.x, current.y + 1);
+
+        return current;
+    }
 }
